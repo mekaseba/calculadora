9cea50151dae2c3bac4675aeb9ebf97dfec0aa92
Meka Seba <mekasebameka@gmail.com>
2022-07-11 22:41:23 -0700

    divide and subtract won't work, can you see the issue in my code? I tried in a server too and it didn't work still. Te amo, my comments on my previous repo was to the code n course, not you.. Even then, I wasn't thinking you when I type it. I was thinking this situation n at that momentito, the code.

---
+++ index.js
@@ -0,0 +1,27 @@
+let one = 8
+let two = 2
+document.getElementById("one-el").textContent = one
+document.getElementById("two-el").textContent = two
+
+
+let sumEl = document.getElementById("sum-el")
+
+function add() {
+let result = one + two
+sumEl.textContent = "Answer = " + result
+}
+
+function sub() {
+let result = one - two 
+sumEl.textcontent = "Answer = " + result 
+}
+
+function mul() {
+let result = one * two 
+sumEl.textContent = "Answer = " + result 
+}
+
+function div() {
+let result = one / two
+sumEl.textcontent = "Answer = " + result 
+}
